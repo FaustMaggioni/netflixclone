81fb81293f385b1f90a78980e39b1b0d8a9d7ac5
Faustino <faustino.maggioni@est.fi.uncoma.edu.ar>
2021-10-04 18:19:59 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.scss';
+
+import { Home } from './routes';
+
+const App:React.FC = () => {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ frontend/src/routes/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./home.scss";
+
+import { Navbar } from '../../components';
+const Home:React.FC = () => {
+    return (
+        <div className="home">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Home;
